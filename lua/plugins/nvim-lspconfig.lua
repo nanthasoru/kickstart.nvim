ab9a33989a8d18f7355d5b24ee8483c0b0c31d74
nanthasoru <nanthasoru@gmail.com>
2025-07-15 09:14:32 +0200

    better indent and automatic install of jdtls and rust-analyzer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -196,7 +196,8 @@ return {
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {},
+      jdtls = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
